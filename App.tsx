1b732fee47a3d67d10d99ad38084657c916e9c8f
jpcirqueira <jpcirqueira81@gmail.com>
2021-06-23 11:35:42 -0300

    create head for register.

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 
-import Dashboard from './src/pages/Dahsboard'; 
+import Register from './src/pages/Register'; 
 import theme from './src/global/styles/theme';
 
 export default function App() {
@@ -25,7 +25,7 @@ export default function App() {
   
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { 
+  Container,
+  Header,
+  Title,
+} from './styles';
+
+const Register: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+    </Container>
+  );
+}
+
+export default Register;
